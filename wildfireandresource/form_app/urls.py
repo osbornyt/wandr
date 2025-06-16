56efbc5574391df2fb6c367bcbc68be46704dc6c
Osborn Yator Ng'imor <osbornyat@gmail.com>
2025-06-16 07:58:01 +0300

    Clear

--- wildfireandresource/form_app/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from form_app import views
-
-urlpatterns = [
-    path('shift-ticket/<int:user_form_id>/', views.generate_shift_ticket_form, name='generate_shift_ticket_form'),
-    path('check-filled/<int:user_form_id>/', views.are_filled_check, name='check_filled'),
-    path('evaluation/<int:user_form_id>/', views.generate_evaluation_form, name='generate_evaluation_form'),
-]
